02907c26d1fb99dd43366da35c844bf553273d74
StepanusRio <stepanusriodefaardian@gmail.com>
2023-03-17 23:54:31 +0700

    How to Get Body Request

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const http = require('http');
-const requestListener = (request,response)=>{
-    response.setHeader('Content-Type','text/html');
-    response.statusCode=200;
-    const {method} = request;
-    if(method == 'GET'){
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+    const { method } = request;
+    if (method == 'GET') {
         response.end('<h1>Hello!</h1>')
     }
-    if(method === 'POST') {
-        response.end('<h1>Hai!</h1>');
-    }
-    if(method === 'PUT') {
-        response.end('<h1>Bonjour!</h1>');
-    }
-    if(method === 'DELETE') {
-        response.end('<h1>Salam!</h1>');
+    if (method === 'POST') {
+        let body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const {name} = JSON.parse(body);
+            response.end(`<h1>Hai, ${name}!</h1>`);
+        });
     }
 };
 const server = http.createServer(requestListener);
@@ -21,6 +25,6 @@ const server = http.createServer(requestListener);
 const port = 5000;
 const host = 'localhost';
 
-server.listen(port,host,()=>{
+server.listen(port, host, () => {
     console.log(`Server Running on http://${host}:${port}`);
 });
