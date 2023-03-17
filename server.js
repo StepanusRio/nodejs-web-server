d9a00971a38b71743ebbff89949c1088bdfb2997
StepanusRio <stepanusriodefaardian@gmail.com>
2023-03-17 18:18:20 +0700

    Create a Native HTTP Server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Halo, kita akan belajar membuat server');
+const http = require('http');
+const requestListener = (request,response)=>{
+    response.setHeader('Content-Type','text/html');
+    response.statusCode=200;
+    response.end('<h1>Hallo Server Http</h1>')
+}
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port,host,()=>{
+    console.log(`Server Running on http://${host}:${port}`);
+});
